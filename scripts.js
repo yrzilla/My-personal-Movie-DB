0b07dcedc6aa689ecb284a23ff660327a1041199
Yuriy Surodeev <51272228+yrzilla@users.noreply.github.com>
2020-07-17 14:22:22 +0300

    Создал метод для проверки приватности, Изменения в методе writeYourGenres
    В методе writeYourGenres запретить пользователю нажать кнопку "отмена" или оставлять пустую строку.

--- scripts.js
+++ scripts.js
@@ -43,14 +43,36 @@ const personalMovieDB = {
     },
     writeYourGenres: function() {
         for (let i = 1; i <= 3; i++) {
-            personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр ${i}`);
+            let genre = prompt(`Ваш любимый жанр ${i}`);
+
+            if(genre == "" || genre == null) {
+                console.log('Вы ввели некорректные данные');
+                i--;
+            } else {
+                personalMovieDB.genres[i - 1] = genre;
+            }
         }
+
+        // !! Альтернативный метод
+        // let genre = prompt("Введите ваши любимые жанры через запятую").toLocaleLowerCase;
+
+        //     if(genre == "" || genre == null) {
+        //         console.log('Вы ввели некорректные данные');
+        //         i--;
+        //     } else {
+        //         personalMovieDB.genres = genre.split(', ');
+        //         personalMovieDB.genres.sort();
+        //     }
+
+        personalMovieDB.genres.forEach((item, i) => {
+            console.log(`Любимый жанр №${i+1} - это ${item}`);
+        });
     },
     toogleVisibleMyDB: function() {
-        if(privat == false) {
-            privat = true;
-        } else if (privat == true) {
-            privat = false;
+        if(personalMovieDB.privat) {
+            personalMovieDB.privat = false;
+        } else {
+            personalMovieDB.privat = true;
         }
     }
 }
